6b8ea6c37040760e3c155e018ae8dd1aee60972a
XuTongle <xutongle@msn.com>
2021-09-13 16:05:44 +0800

    c

--- src/Models/Refund.php
+++ src/Models/Refund.php
@@ -41,9 +41,7 @@ use think\model\relation\BelongsTo;
  */
 class Refund extends Model
 {
-    use SoftDelete;
-    use Traits\DateTimeFormatter;
-    use Traits\UsingDatetimeAsPrimaryKey;
+    use SoftDelete, Traits\DateTimeFormatter, Traits\UsingDatetimeAsPrimaryKey;
 
     // 退款状态机
     public const STATUS_PENDING = 'PENDING';//待处理
